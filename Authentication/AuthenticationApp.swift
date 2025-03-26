8825566496ef88d529a740ee6968ee6f20ac39b0
Ian Kiprotich <kiprotichian254@gmail.com>
2025-03-26 11:29:29 +0300

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Ian   on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
